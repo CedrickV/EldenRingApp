c330b8d094e1b40c44e2c14dfaa3caa2d85a2e81
Cédrick leRoi <cedrickdavidvernon@gmail.com>
2023-02-25 23:32:09 +0100

    "swag commit 9111"

--- app/src/main/java/com/example/eldenringapp/MainActivity.java
+++ app/src/main/java/com/example/eldenringapp/MainActivity.java
@@ -3,14 +3,25 @@ package com.example.eldenringapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // initialise the layout
+        webView = findViewById(R.id.webvidew);
+
+        // enable the javascript to load the url
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        // add the url of gif
+        webView.loadUrl("gif/");
 
 
     }
