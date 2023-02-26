a7a7129b65382bb8dd27033174caa3c178b48608
Josue Martial <josue97255@gmail.com>
2023-02-26 05:13:15 +0100

    Armes

---
+++ app/src/main/java/com/example/eldenringapp/Armes/Armes.java
@@ -0,0 +1,31 @@
+package com.example.eldenringapp.Armes;
+
+public class Armes {
+    private String name;
+    private String description;
+    private String armeUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getArmeUrl() {
+        return armeUrl;
+    }
+
+    public void setArmeUrl(String armeUrl) {
+        this.armeUrl = armeUrl;
+    }
+}
--- app/src/main/java/com/example/eldenringapp/MainActivity.java
+++ app/src/main/java/com/example/eldenringapp/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.eldenringapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
 import com.bumptech.glide.Glide;
+import com.example.eldenringapp.Armes.ArmesActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +21,14 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.class_imageView);
         Glide.with(this).load(R.raw.eldenring).into(imageView);
 
-
+        Button buttonArmes = findViewById(R.id.button_Armes);
+        buttonArmes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ArmesActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
