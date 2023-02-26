6a8c972b4f3f86bc1a023dc38e8e4292267408c0
Cédrick leRoi <cedrickdavidvernon@gmail.com>
2023-02-26 03:27:32 +0100

    "swag commit mann"

--- app/src/main/java/com/example/eldenringapp/MainActivity.java
+++ app/src/main/java/com/example/eldenringapp/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.eldenringapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
+import com.example.eldenringapp.Classe.ClasseActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,45 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.classe_imageView);
         Glide.with(this).load(R.raw.eldenring).into(imageView);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Button buttonClasse = findViewById(R.id.button_Act3);
+        buttonClasse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, ClasseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
